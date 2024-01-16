7ff6b0973f32067f5858484d37d3c111b2b41aa1
Vivek Srivastava <viveksrivastava6180@gmail.com>
2024-01-16 21:45:36 +0530

    refactor: Prettify code

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,28 +1,28 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
-import MultiSelect from "../MultiSelect"
-import { Option } from "../MultiSelect/MultiSelect"
-import style from './Form.module.scss'
+import { useEffect, useState } from "react";
+import MultiSelect from "../MultiSelect";
+import { Option } from "../MultiSelect/MultiSelect";
+import style from "./Form.module.scss";
 
 export default function Form() {
-    const [values, setValues] = useState<Option[]>([])
-    const [options, setOptions] = useState<Option[]>([])
-    const HandleChange = (values: Option[]) => {
-        setValues(values)
-    }
+  const [values, setValues] = useState<Option[]>([]);
+  const [options, setOptions] = useState<Option[]>([]);
+  const HandleChange = (values: Option[]) => {
+    setValues(values);
+  };
 
-    useEffect(()=>{
-        (async ()=>{
-            const res = await fetch('/api/getOptions');
-            const response= await res.json()
-            setOptions(response)
-        })()
-    }, [])
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("/api/getOptions");
+      const response = await res.json();
+      setOptions(response);
+    })();
+  }, []);
 
-    return (
-        <div className={style.form}>
-            <MultiSelect options={options} values={values} onChange={HandleChange} />
-        </div>
-    )
+  return (
+    <div className={style.form}>
+      <MultiSelect options={options} values={values} onChange={HandleChange} />
+    </div>
+  );
 }
--- components/MultiSelect/MultiSelect.tsx
+++ components/MultiSelect/MultiSelect.tsx
@@ -1,98 +1,134 @@
-import { ChangeEvent, KeyboardEvent, useEffect, useMemo, useRef, useState } from 'react';
-import style from './MultiSelect.module.scss'
-
-import { RxCross1 } from "react-icons/rx";
-import Avatar from '../Avatar';
-
-export type Option = {
+import {
+    ChangeEvent,
+    KeyboardEvent,
+    useEffect,
+    useMemo,
+    useRef,
+    useState,
+  } from "react";
+  import style from "./MultiSelect.module.scss";
+  
+  import { RxCross1 } from "react-icons/rx";
+  import Avatar from "../Avatar";
+  
+  export type Option = {
     label: string;
     value: string | number;
-    icon: string
-}
-
-interface MultiSelectOptions {
-    placeholder?: string,
+    icon: string;
+  };
+  
+  interface MultiSelectOptions {
+    placeholder?: string;
     options: Option[];
     values: Option[];
-    onChange: (value: Option[]) => void
-}
-
-const isValueNotInArray = (value: string | number, array: Option[]) => !array.some(item => item.value === value);
-const doesLabelMatchInput = (label: string, input: string) => label.toLowerCase().includes(input.toLowerCase());
-
-export default function MultiSelect({ placeholder = "Type Something...", options = [], values = [], onChange }: MultiSelectOptions) {
+    onChange: (value: Option[]) => void;
+  }
+  
+  const isValueNotInArray = (value: string | number, array: Option[]) =>
+    !array.some((item) => item.value === value);
+  const doesLabelMatchInput = (label: string, input: string) =>
+    label.toLowerCase().includes(input.toLowerCase());
+  
+  export default function MultiSelect({ placeholder = "Type Something...", options = [], values = [], onChange }: MultiSelectOptions) {
     const [isOpen, setIsOpen] = useState<boolean>(false);
-    const [input, setInput] = useState<string>('');
+    const [input, setInput] = useState<string>("");
     const [highlight, setHighlight] = useState(false);
     const parentRef = useRef<HTMLDivElement>(null);
     const inputRef = useRef<HTMLDivElement>(null);
-
+  
     const filteredOptions: Option[] = useMemo(() => {
-        return options.filter((option) => {
-            return isValueNotInArray(option.value, values) && doesLabelMatchInput(option.label, input);
-        });
-    }, [options, values, input])
-
+      return options.filter((option) => {
+        return (
+          isValueNotInArray(option.value, values) &&
+          doesLabelMatchInput(option.label, input)
+        );
+      });
+    }, [options, values, input]);
+  
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setInput(e.target.innerHTML)
-        setHighlight(false)
-    }
+      setInput(e.target.innerHTML);
+      setHighlight(false);
+    };
     const handleItemClick = (option: Option) => {
-        onChange([...values, option])
-        setHighlight(false)
-    }
+      onChange([...values, option]);
+      setHighlight(false);
+    };
     const removeItem = (option: Option) => {
-        onChange(values.filter(({ value }) => value !== option.value))
-        setHighlight(false)
-    }
-
+      onChange(values.filter(({ value }) => value !== option.value));
+      setHighlight(false);
+    };
+  
     const handleKeyPress = (e: KeyboardEvent<HTMLDivElement>) => {
-        if (e.key === 'Backspace') {
-            if (!highlight) return setHighlight(true);
-            values.pop()
-            onChange([...values]);
-            setHighlight(false);
-        }
-    }
-
+      if (e.key === "Backspace") {
+        if (!highlight) return setHighlight(true);
+        values.pop();
+        onChange([...values]);
+        setHighlight(false);
+      }
+    };
+  
     useEffect(() => {
-        const onClickListener = (e: MouseEvent) => {
-            if (parentRef.current?.contains(e.target as HTMLDivElement)) {
-                inputRef?.current?.focus();
-                setIsOpen(true);
-            }
-            else {
-                inputRef?.current?.blur();
-                setIsOpen(false);
-            }
+      const onClickListener = (e: MouseEvent) => {
+        if (parentRef.current?.contains(e.target as HTMLDivElement)) {
+          inputRef?.current?.focus();
+          setIsOpen(true);
+        } else {
+          inputRef?.current?.blur();
+          setIsOpen(false);
         }
-
-        document.body.addEventListener('click', onClickListener)
-        return () => document.body.removeEventListener('click', onClickListener);
-    })
-
-
-
+      };
+  
+      document.body.addEventListener("click", onClickListener);
+      return () => document.body.removeEventListener("click", onClickListener);
+    });
+  
     return (
-        <div ref={parentRef} onKeyDown={handleKeyPress} className={style.select}>
-            <div className={style.chipContainer}>
-                {
-                    values.map((value, i) => {
-                        return <div className={`${style.chip} ${highlight && i === values.length - 1 ? style.highlight : ''}`} key={i}>
-                            <Avatar className={style.avatar} icon={value.icon} name={value.label} />{value.label}<RxCross1 className={style.remove} onClick={() => removeItem(value)} />
-                        </div>
-                    })
-                }
-                <div ref={inputRef} className={`${style.input} ${!values.length ? style.placeholder : ''}`} style={{ paddingLeft: values.length ? 0 : 8 }} onInput={handleInputChange} contentEditable aria-placeholder={placeholder} />
-            </div>
-            {
-                isOpen && <div className={style.optionsContainer}><div className={style.optionsWindow}>
-                    {filteredOptions.length > 0 ? filteredOptions.map((option, i) => {
-                        return <div className={style.option} onClick={() => handleItemClick(option)} key={i}><Avatar className={style.avatar} icon={option.icon} name={option.label} />{option.label}</div>
-                    }) : <div className={style.resultNotFound}>No Results found</div>
-                    }
-                </div></div>
-            }
+      <div ref={parentRef} onKeyDown={handleKeyPress} className={style.select}>
+        <div className={style.chipContainer}>
+          {values.map((value, i) => {
+            return (
+              <div
+                className={`${style.chip} ${
+                  highlight && i === values.length - 1 ? style.highlight : ""
+                }`}
+                key={i}
+              >
+                <Avatar className={style.avatar} icon={value.icon} name={value.label} />
+                {value.label}
+                <RxCross1 className={style.remove} onClick={() => removeItem(value)}
+                />
+              </div>
+            );
+          })}
+          <div
+            ref={inputRef}
+            className={`${style.input} ${!values.length ? style.placeholder : ""}`}
+            style={{ paddingLeft: values.length ? 0 : 8 }}
+            onInput={handleInputChange}
+            contentEditable
+            aria-placeholder={placeholder}
+          />
         </div>
-    )
-}
+        {isOpen && (
+          <div className={style.optionsContainer}>
+            <div className={style.optionsWindow}>
+              {filteredOptions.length > 0 ? (
+                filteredOptions.map((option, i) => {
+                  return (
+                    <div
+                      className={style.option} onClick={() => handleItemClick(option)} key={i} >
+                      <Avatar className={style.avatar} icon={option.icon} name={option.label} />
+                      {option.label}
+                    </div>
+                  );
+                })
+              ) : (
+                <div className={style.resultNotFound}>No Results found</div>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+  
